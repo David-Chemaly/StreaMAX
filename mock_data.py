3c378f4e39ca62660f7629fe195c130922b18f60
David-Chemaly <davidchemaly@hotmail.com>
2025-07-27 17:09:57 +0100

    can generate mock nice looking streams

--- mock_data.py
+++ mock_data.py
@@ -1,38 +1,77 @@
+from models import generate_stream_spray
 import scipy
 
 import jax
 import jax.numpy as jnp
 import jax.random as random
+from utils import get_q
+from priors import sample_params
+from utils import get_track
 
-@jax.jit
-def sample_params_data(seed):
-    # seed = np.random.randint(0, 2**32 - 1)  # Ensure it's within JAX's valid range
+import numpy as np
 
-    key = random.PRNGKey(seed)  # Set seed for reproducibility
-    # Split key once for all parameters
-    keys = random.split(key, 10)  # Generate enough subkeys at once
+# @jax.jit
+def get_data(seed, sigma=2):
+    is_data = False
+    rng = np.random.default_rng(seed)
 
-    # Generate random variables
-    logM = random.uniform(keys[0], shape=(), minval=11, maxval=14)
-    Rs   = random.uniform(keys[1], shape=(), minval=5, maxval=25)
-    dirx, diry, dirz = random.normal(keys[3], shape=(3,))    # Mean 0, Std 1
-    dirz = jnp.abs(dirz)  # Ensure positive direction
+    while not is_data:
+        seed_here = rng.integers(0, 2**32-1)
+        params = sample_params(seed_here)
 
-    logm = random.uniform(keys[4], shape=(), minval=7, maxval=9)
-    rs   = random.uniform(keys[5], shape=(), minval=1, maxval=3)
+        q = get_q(params[2], params[3], params[4])
+        theta_stream, xv_stream = generate_stream_spray(params[:2] + (q, ) + params[2:],  seed=111, n_steps=500, n_particles=10000)
+        count, theta_bin, r_bin, w_bin = get_track(theta_stream, xv_stream[:, 0], xv_stream[:, 1])
 
-    # Generate normal-distributed variables
-    x0, z0 = random.normal(keys[6], shape=(2,)) * 150     # Mean 0, Std 50
-    x0 = jnp.abs(x0)  # Ensure positive position
-    z0 = jnp.abs(z0)
-    y0 = 0. # Set to 0
+        crit1 = jnp.nanmin(r_bin) > 10  # Must be further than 10kpc minimum
+        crit2 = jnp.sum(jnp.where(count > 100, 1, 0)) > 9 # Must have at least 10 bins with more than 100 particles
+        crit3 = jnp.nansum(r_bin[:-1]*jnp.tanh(jnp.diff(theta_bin))) > 100 # Must have length of at least 100kpc
 
-    vx0, vy0, vz0 = random.normal(keys[7], shape=(3,)) * 250  # Mean 0, Std 50
-    vy0 = jnp.abs(vy0)
+        if crit1 and crit2 and crit3:
+            is_data = True
 
-    # Generate time
-    time  = random.uniform(keys[8], shape=(), minval=1, maxval=4)
-    alpha = random.uniform(keys[9], shape=(), minval=0.9, maxval=1.1)
+    dict_data = {
+        'params': params,
+        'theta_stream': theta_stream,
+        'x_stream': xv_stream[:, 0],
+        'y_stream': xv_stream[:, 1],
+        'count': count,
+        'theta_bin': theta_bin,
+        'r_bin': r_bin,
+        'w_bin': w_bin
+    }
 
-    return logM, Rs, dirx, diry, dirz, logm, rs, x0, y0, z0, vx0, vy0, vz0, time, alpha
+    return dict_data
 
+# def get_data_stream(q_true, seed, sigma=1, tail=0, min_count=11, n_theta_min=13, r_min=20, r_max=500, l_min=200, R2_max=0.8):
+#     is_data = False
+#     rng = np.random.default_rng(seed)
+#     theta_gap = np.diff(np.linspace(-2*np.pi, 2*np.pi, N_BINS))[0]
+#     while not is_data:
+#         logM, Rs, q, dirx, diry, dirz, logm, rs, x0, y0, z0, vx0, vy0, vz0, time, alpha = sample_params_data(q_true, rng.integers(0, 2**32-1))
+
+#         theta_stream, x_stream, y_stream, vz_stream, r_meds, w_meds, x_meds, y_meds, vz_meds = jax_stream_model(logM, Rs, q, dirx, diry, dirz, logm, rs, x0, y0, z0, vx0, vy0, vz0, time, alpha, tail, min_count)
+
+#         if np.sum(~np.isnan(r_meds)) > n_theta_min:
+
+#             crit1 = np.all(np.diff(np.where(~np.isnan(r_meds))) == 1) # Must be continuous
+#             crit2 = np.nanmin(r_meds) > r_min # Must be further than 10kpc minimum
+#             crit3 = np.sum(r_meds[~np.isnan(r_meds)][:-1] * np.tan(theta_gap)) > l_min # Must have length of at least 100kpc
+#             crit4 = np.corrcoef(x_meds[~np.isnan(x_meds)], y_meds[~np.isnan(y_meds)])[0, 1]**2 <= R2_max # Can't be too much a straight line
+#             crit5 = np.nanmax(r_meds) < r_max # Must be less than 500kpc
+#             crit6 = np.nanmin((x_stream**2 + y_stream**2)**0.5) > r_min/4
+
+#             if crit1 and crit2 and crit3 and crit4 and crit5 and crit6:
+#                 is_data = True
+
+#     params = np.array([logM, Rs, q, dirx, diry, dirz, logm, rs, x0, z0, vx0, vy0, vz0, time, alpha])
+
+#     r_err = np.abs(r_meds)*sigma/100
+#     r_noise = rng.normal(0, r_err)
+
+#     w_err = np.abs(w_meds)*sigma/100
+#     w_noise = rng.normal(0, w_err)
+
+#     dict_data = {'params': params, 'theta_stream': theta_stream, 'x_stream': x_stream, 'y_stream': y_stream, 'x_meds': x_meds, 'y_meds': y_meds, 'r_data': r_meds+r_noise, 'r_err': r_err, 'r_noise': r_noise, 'w_data': w_meds+w_noise, 'w_err': w_err, 'w_noise': w_noise, 'params_time':time}
+
+#     return dict_data
---
+++ priors.py
@@ -0,0 +1,68 @@
+import jax
+import jax.numpy as jnp
+import jax.random as random
+
+@jax.jit
+def sample_params(seed):
+    # seed = np.random.randint(0, 2**32 - 1)  # Ensure it's within JAX's valid range
+
+    key = random.PRNGKey(seed)  # Set seed for reproducibility
+    # Split key once for all parameters
+    keys = random.split(key, 10)  # Generate enough subkeys at once
+
+    # Generate random variables
+    logM = random.uniform(keys[0], shape=(), minval=11, maxval=14)
+    Rs   = random.uniform(keys[1], shape=(), minval=5, maxval=25)
+    dirx, diry, dirz = random.normal(keys[3], shape=(3,))    # Mean 0, Std 1
+    dirz = jnp.abs(dirz)  # Ensure positive direction
+
+    logm = random.uniform(keys[4], shape=(), minval=7, maxval=9)
+    rs   = random.uniform(keys[5], shape=(), minval=1, maxval=3)
+
+    # Generate normal-distributed variables
+    x0, z0 = random.normal(keys[6], shape=(2,)) * 150     # Mean 0, Std 50
+    x0 = jnp.abs(x0)  # Ensure positive position
+    z0 = jnp.abs(z0)
+    y0 = 0. # Set to 0
+
+    vx0, vy0, vz0 = random.normal(keys[7], shape=(3,)) * 250  # Mean 0, Std 50
+    vy0 = jnp.abs(vy0)
+
+    # Generate time
+    time  = random.uniform(keys[8], shape=(), minval=1, maxval=4)
+    alpha = random.uniform(keys[9], shape=(), minval=0.9, maxval=1.1)
+
+    return logM, Rs, dirx, diry, dirz, logm, rs, x0, y0, z0, vx0, vy0, vz0, time, alpha
+
+@jax.jit
+def prior_transform(p):
+    logM, Rs, dirx, diry, dirz, \
+    logm, rs, \
+    x0, z0, vx0, vy0, vz0, \
+    t0, a0 = p
+
+    logM1 = 11 + 3 * logM
+    Rs1   = 5 + 20 * Rs
+
+    dirx1 = jax.scipy.special.ndtri(dirx)
+    diry1 = jax.scipy.special.ndtri(diry)
+    dirz1 = jax.scipy.special.ndtri(0.5 + dirz / 2)
+
+    logm1 = 7 + 2 * logm
+    rs1   = 1 + 2 * rs
+
+    x1 = jax.scipy.special.ndtri(0.5 + x0 / 2) * 150
+    z1 = jax.scipy.special.ndtri(0.5 + z0 / 2) * 150
+
+    vx1 = jax.scipy.special.ndtri(vx0) * 250
+    vy1 = jax.scipy.special.ndtri(0.5 + vy0 / 2) * 250
+    vz1 = jax.scipy.special.ndtri(vz0) * 250
+
+    t1 = 1 + 3 * t0
+    a1 = 0.9 + 0.2 * a0
+    return jnp.array([
+        logM1, Rs1, dirx1, diry1, dirz1,
+        logm1, rs1,
+        x1, z1, vx1, vy1, vz1,
+        t1, a1
+    ])
